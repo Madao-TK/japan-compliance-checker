316bf1e07aed412e14e045b8e18712ad4c96435a
Madao_TK <future.of.contributors.tk@gmail.com>
2025-10-28 22:55:20 +0900

    feat: Add minimal index.js page to resolve 404 issue

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>🏠 防災地図サービス (開発中)</h1>
+      <p>このサイトは現在開発中です。データ処理APIは /api/disaster-data で稼働しています。</p>
+    </div>
+  );
+};
+
+export default Home;
